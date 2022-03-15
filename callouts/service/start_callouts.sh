220a3c2d85673d2b73dd5281905e6b15b941fefb
clayton <cmyager>
2022-03-15 09:57:40 -0700

    fixed start scripts

--- callouts/service/start_callouts.sh
+++ callouts/service/start_callouts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/callouts/original_callouts
+cd /home/callouts/original_callouts/callouts
 # source /home/callouts/spirit/.new/bin/activate
-source /home/callouts/original_callouts/.venv/bin/activate
+source /home/callouts/original_callouts/callouts/.venv/bin/activate
 export PYTHONDONTWRITEBYTECODE=1
 python -B callouts.py
--- website/service/start_calloutswebsite.sh
+++ website/service/start_calloutswebsite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/callouts/original_callouts/website
-source /home/callouts/original_callouts/website/.env/bin/activate
+source /home/callouts/original_callouts/website/.venv/bin/activate
 export PYTHONDONTWRITEBYTECODE=1
 export OAUTHLIB_INSECURE_TRANSPORT=1
 export FLASK_RUN_PORT=5000
-.env/bin/hypercorn -w 3 flask_app:app
+.venv/bin/hypercorn -w 3 flask_app:app
